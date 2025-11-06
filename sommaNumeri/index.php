4d8a573c4bde78aea7f2dcc1e57c07bf28b33fe4
edpoli <edpoli96@gmail.com>
2025-11-06 17:01:53 +0100

    nuovo esercizio

---
+++ sommaNumeri/index.php
@@ -0,0 +1,30 @@
+<?php
+
+
+$array = [1,2,3,4,5,6];
+$somma = 0;
+
+foreach($array as $numero ) {
+
+$somma += $numero;
+
+};
+
+echo $somma;
+
+
+?>
+
+
+
+<?php
+
+/*
+$array = [1,2,3,4,5,6];
+
+echo array_sum($array);
+*/
+
+
+
+?>
